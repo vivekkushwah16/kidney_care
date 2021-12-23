d9f8325b83e47af59064153e3b28c1c3cb3c7aa5
vivekkushwah16 <vivekushwah714@gmail.com>
2021-12-23 12:20:32 +0530

    footer update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,41 +1,42 @@
 import React, { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import {  useHistory } from "react-router-dom";
 import classes from "./style.module.css";
-import logo from "../../assets/images/Group 824.png";
-import downArrow from "../../assets/images/Polygon 1.png";
-import profileImage from "../../assets/images/Ellipse 8(1).png";
 const Footer = () => {
-  const [openNav, setNav] = useState(false);
-  const location = useLocation();
-  console.log(location)
-  console.log(location.pathname)
+  const[selectOption,setOption]=useState("home")
+  // const {location} = useHistory();
+  const history = useHistory();
+  console.log(history)
+  function handleSwitchPage(url){
+   history.push(`/${url}`)
+   setOption(url)
+  }
   return (
     <>
       <section className={`${classes.FooterWrapper}`}>
         <nav className={`${classes.footer} container`}>
           <div className={classes.content}>
             <div className={classes.icon}>
-              <i className={location.pathname === "/" ? "icon-home-outline" : "icon-home"}></i>
+              <i className={selectOption === "home" ? "icon-home-outline" : "icon-home"}></i>
             </div>
-            <h1 className={location.pathname === "/" ? classes.textBold : ""}>Home</h1>
+            <h1 className={selectOption === "home" ? classes.textBold : ""}>Home</h1>
           </div>
-          <div className={classes.content}>
+          <div className={classes.content} onClick={() => handleSwitchPage("events")}>
             <div className={classes.icon}>
-              <i  className={location.pathname === "/events" ? "icon-events-outline" :"icon-events"}></i>
+              <i  className={selectOption === "events" ? "icon-events-outline" :"icon-events"}></i>
             </div>
-            <h1 className={location.pathname === "/events" ? classes.textBold : ""}>Events</h1>
+            <h1 className={selectOption === "events" ? classes.textBold : ""}>Events</h1>
           </div>
-          <div className={classes.content}>
+          <div className={classes.content} >
             <div className={classes.icon}>
               <i className="icon-notification-outline"></i>
             </div>
-            <h1 className={location.pathname === "/notification" ? classes.textBold : ""}>Notification</h1>
+            <h1 className={selectOption === "notification" ? classes.textBold : ""}>Notification</h1>
           </div>
-          <div className={classes.content}>
+          <div className={classes.content} >
             <div className={classes.icon}>
               <i className="icon-profile-outline"></i>
             </div>
-            <h1 className={location.pathname === "/profile" ? classes.textBold : ""}>Profile</h1>
+            <h1 className={selectOption === "profile" ? classes.textBold : ""}>Profile</h1>
           </div>
         </nav>
       </section>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 import classes from "./style.module.css";
 import logo from "../../assets/images/Group 824.png";
 import downArrow from "../../assets/images/Polygon 1.png";
 import profileImage from "../../assets/images/Ellipse 8(1).png";
 const Navbar = () => {
   const [openNav, setNav] = useState(false);
-  const location = useLocation();
-  console.log(location)
-  console.log(location.pathname)
   return (
     <>
       <section className={classes.Wrapper}>
